56040fa50cf6f56e830bd19386be028e5a6f725b
unknown <matthiez@gmail.com>
2024-04-16 09:06:01 +0200

    rename lookup getter

--- seven_api/resources/LookupResource.py
+++ seven_api/resources/LookupResource.py
@@ -4,21 +4,21 @@ from seven_api.resources.Resource import Resource
 
 class LookupResource(Resource):
     def cnam(self, number: str) -> list:
-        return self.__post('cnam', number)
+        return self.__get('cnam', number)
 
     def hlr(self, params=None) -> list:
-        return self.__post('hlr', params)
+        return self.__get('hlr', params)
 
     def mnp(self, params=None) -> list:
-        return self.__post('mnp', params)
+        return self.__get('mnp', params)
 
     def format(self, params=None) -> list:
-        return self.__post('format', params)
+        return self.__get('format', params)
 
     def rcs(self, params=None) -> list:
-        return self.__post('rcs', params)
+        return self.__get('rcs', params)
 
-    def __post(self, lookup_type: str, number: str) -> list:
+    def __get(self, lookup_type: str, number: str) -> list:
         res = self._client.get('{}/{}?number={}'.format(Endpoint.LOOKUP.value, lookup_type, number))
         
         if isinstance(res, dict):
