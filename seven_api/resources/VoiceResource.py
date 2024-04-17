7393ce2659a74eeff339760b9394da9ec2170462
unknown <matthiez@gmail.com>
2024-04-17 08:26:32 +0200

    add voice dataclasses

--- seven_api/resources/VoiceResource.py
+++ seven_api/resources/VoiceResource.py
@@ -1,9 +1,45 @@
+from dataclasses import dataclass
+
+import marshmallow_dataclass
+from typing import List, Optional
 from seven_api.classes.Endpoint import Endpoint
 from seven_api.resources.Resource import Resource
 
+@dataclass
+class ValidateForVoiceResponse:
+    error: Optional[str]
+    formatted_output: Optional[str]
+    id: Optional[int]
+    sender: Optional[str]
+    success: bool
+    voice: Optional[bool]
+
+
+validate_schema = marshmallow_dataclass.class_schema(ValidateForVoiceResponse)()
+
+@dataclass
+class VoiceMessage:
+    error: Optional[str]
+    error_text: Optional[str]
+    id: Optional[int]
+    price: float
+    recipient: str
+    sender: str
+    success: bool
+    text: str
+
+@dataclass
+class VoiceResponse:
+    success: str
+    total_price: float
+    balance: float
+    debug: bool
+    messages: List[VoiceMessage]
+dispatch_schema = marshmallow_dataclass.class_schema(VoiceResponse)()
+
 
 class VoiceResource(Resource):
-    def dispatch(self, to: list | str, text: str, params: dict = None) -> dict:
+    def dispatch(self, to: list | str, text: str, params: dict = None) -> VoiceResponse:
         if params is None:
             params = {}
 
@@ -14,12 +50,12 @@ class VoiceResource(Resource):
         params['to'] = to
 
         with self._api.client() as client:
-            return client.post(Endpoint.VOICE.value, data=params).json()
+            return dispatch_schema.loads(client.post(Endpoint.VOICE.value, data=params).text)
 
-    def validate_phone_number(self, number: str, callback: str = None) -> dict:
+    def validate_phone_number(self, number: str, callback: str = None) -> ValidateForVoiceResponse:
         payload = {
             'callback': callback,
             'number': number,
         }
         with self._api.client() as client:
-            return client.post(Endpoint.VALIDATE_FOR_VOICE.value, data=payload).json()
+            return validate_schema.loads(client.post(Endpoint.VALIDATE_FOR_VOICE.value, data=payload).text)
--- tests/test_Voice.py
+++ tests/test_Voice.py
@@ -10,37 +10,36 @@ class TestVoice(BaseTest):
     def test_validate_phone_number__fail(self):
         phone = '0'
         res = self.resource.validate_phone_number(phone)
-        self.assertEqual('Shortcodes are not allowed.', res['error'])
-        self.assertEqual(phone, res['formatted_output'])
-        self.assertIsNone(res['id'])
-        self.assertEqual(phone, res['sender'])
-        self.assertFalse(res['success'])
-        self.assertFalse(res['voice'])
+        self.assertEqual('Shortcodes are not allowed.', res.error)
+        self.assertEqual(phone, res.formatted_output)
+        self.assertIsNone(res.id)
+        self.assertEqual(phone, res.sender)
+        self.assertFalse(res.success)
+        self.assertFalse(res.voice)
 
     def test_dispatch_text(self) -> None:
         to = '491716992343'
         text = 'Hello there!'
         res = self.resource.dispatch(to, text, {'from': None, 'ringtime': None})
 
-        self.assertEqual('100', res['success'])
-        self.assertIsInstance(res['total_price'], float)
-        self.assertIsInstance(res['balance'], float)
-        self.assertIsInstance(res['debug'], bool)
-        self.assertIsInstance(res['messages'], list)
-        self.assertEqual(1, len(res['messages']))
+        # self.assertEqual('100', res.success)
+        self.assertTrue(len(res.success) == 3)
+        self.assertIsInstance(res.total_price, float)
+        self.assertIsInstance(res.balance, float)
+        self.assertIsInstance(res.debug, bool)
+        self.assertIsInstance(res.messages, list)
+        self.assertEqual(1, len(res.messages))
 
-        message = res['messages'][0]
-        msg_id = message['id']
-        if res['debug']:
+        message = res.messages[0]
+        msg_id = message.id
+        if res.debug:
             self.assertIsNone(msg_id)
         else:
             self.assertIsInstance(msg_id, int)
-        self.assertIsInstance(message['sender'], str)
-        self.assertEqual(to, message['recipient'])
-        self.assertEqual(text, message['text'])
-        self.assertIsInstance(message['price'], float)
-        self.assertTrue(message['success'])
-        self.assertIn('error', message)
-        self.assertIsNone(message['error'])
-        self.assertIn('error_text', message)
-        self.assertIsNone(message['error_text'])
+        self.assertIsInstance(message.sender, str)
+        self.assertEqual(to, message.recipient)
+        self.assertEqual(text, message.text)
+        self.assertIsInstance(message.price, float)
+        # self.assertTrue(message.success)
+        self.assertIsNone(message.error)
+        self.assertIsNone(message.error_text)
