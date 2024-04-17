f018205028bcce50206bf06179153e7bc0da7b21
unknown <matthiez@gmail.com>
2024-04-17 08:59:42 +0200

    add sms dataclasses

--- seven_api/resources/SmsResource.py
+++ seven_api/resources/SmsResource.py
@@ -1,18 +1,62 @@
+from dataclasses import dataclass
+from typing import List, Optional
+
+import marshmallow_dataclass
+
 from seven_api.classes.Endpoint import Endpoint
 from seven_api.classes.ExtendedEnum import ExtendedEnum
 from seven_api.resources.Resource import Resource
 
+@dataclass
+class SmsMessage:
+    encoding: str
+    error: Optional[str]
+    error_text: Optional[str]
+    id: Optional[str]
+    is_binary: bool
+    label: Optional[str]
+    parts: int
+    price: float
+    recipient: str
+    sender: str
+    success: bool
+    text: str
+    udh: Optional[str]
+
+
+@dataclass
+class SmsResponse:
+    balance: float
+    debug: str
+    messages: List[SmsMessage]
+    sms_type: str
+    success: str
+    total_price: float
+dispatch_schema = marshmallow_dataclass.class_schema(SmsResponse)()
+
+@dataclass
+class SmsDeleteResponse:
+    deleted: List[str]
+    success: bool
+delete_schema = marshmallow_dataclass.class_schema(SmsDeleteResponse)()
+
+@dataclass
+class SmsStatus:
+    id: str
+    status: Optional[str]
+    status_time: Optional[str]
+status_schema = marshmallow_dataclass.class_schema(SmsStatus)()
 
 class SmsResource(Resource):
-    def delete(self, ids: list | str) -> dict:
+    def delete(self, ids: list | str) -> SmsDeleteResponse:
         if isinstance(ids, str):
             ids = [ids]
         ids = ','.join(ids)
 
         with self._api.client() as client:
-            return client.delete(Endpoint.SMS.value, params={'ids[]': ids}).json()
+            return delete_schema.loads(client.delete(Endpoint.SMS.value, params={'ids[]': ids}).text)
 
-    def dispatch(self, to: list | str, text: str, params: dict = None) -> dict:
+    def dispatch(self, to: list | str, text: str, params: dict = None) -> SmsResponse:
         if params is None:
             params = {}
         params['text'] = text
@@ -21,15 +65,15 @@ class SmsResource(Resource):
         params['to'] = to
 
         with self._api.client() as client:
-            return client.post(Endpoint.SMS.value, data=params).json()
+            return dispatch_schema.loads(client.post(Endpoint.SMS.value, data=params).text)
 
-    def status(self, ids: list | str) -> list:
+    def status(self, ids: list | str) -> List[SmsStatus]:
         if isinstance(ids, str):
             ids = [ids]
         ids = ','.join(ids)
 
         with self._api.client() as client:
-            return client.get(Endpoint.STATUS.value, params={'msg_id': ids}).json()
+            return status_schema.loads(client.get(Endpoint.STATUS.value, params={'msg_id': ids}).text, many=True)
 
 
 class StatusMessage(ExtendedEnum):
--- tests/test_Sms.py
+++ tests/test_Sms.py
@@ -13,51 +13,46 @@ class TestSms(BaseTest):
         self.assertEqual(1, len(entries))
         entry = entries[0]
 
-        if entry['status'] is None:
-            self.assertIsNone(entry['status_time'])
+        if entry.status is None:
+            self.assertIsNone(entry.status_time)
         else:
-            self.assertIn(entry['status'], StatusMessage.names())
-            self.assertTrue(BaseTest.is_valid_datetime(entry['status_time'], "%Y-%m-%d %H:%M:%S.%f"))
+            self.assertIn(entry.status, StatusMessage.names())
+            self.assertTrue(BaseTest.is_valid_datetime(entry.status_time, "%Y-%m-%d %H:%M:%S.%f"))
 
     def test_sms_dispatch__simple(self) -> None:
         to = '491716992343'
         text = 'HEY!!'
         res = self.resource.dispatch(to, text, {'delay': '2050-12-31 12:34:56'})
-        self.assertEqual('100', res['success'])
-        self.assertIsInstance(res['total_price'], float)
-        self.assertIsInstance(res['balance'], float)
-        self.assertIsInstance(res['debug'], str)
-        self.assertIsInstance(res['sms_type'], str)
-        self.assertIsInstance(res['messages'], list)
-        self.assertEqual(1, len(res['messages']))
+        self.assertEqual('100', res.success)
+        self.assertGreaterEqual(res.total_price, 0.0)
+        self.assertIn(res.debug, ['true', 'false'])
+        self.assertEqual('direct', res.sms_type)
+        self.assertEqual(1, len(res.messages))
 
-        message = res['messages'][0]
-        msg_id = message['id']
-        if 'true' == res['debug']:
-            self.assertIsNone(msg_id)
+        message = res.messages[0]
+        if 'true' == res.debug:
+            self.assertTrue(len(message.id) > 0)
         else:
-            self.assertIsInstance(msg_id, str)
-        self.assertIsInstance(message['sender'], str)
-        self.assertEqual(to, message['recipient'])
-        self.assertEqual(text, message['text'])
-        self.assertEqual('gsm', message['encoding'])
-        self.assertEqual(1, message['parts'])
-        self.assertIsInstance(message['price'], float)
-        self.assertTrue(message['success'])
-        self.assertIn('error', message)
-        self.assertIsNone(message['error'])
-        self.assertIn('error_text', message)
-        self.assertIsNone(message['error_text'])
+            self.assertIsInstance(message.id, str)
+        self.assertTrue(len(message.sender) > 0)
+        self.assertEqual(to, message.recipient)
+        self.assertEqual(text, message.text)
+        self.assertEqual('gsm', message.encoding)
+        self.assertEqual(1, message.parts)
+        self.assertGreaterEqual(message.price, 0.0)
+        self.assertTrue(message.success)
+        self.assertIsNone(message.error)
+        self.assertIsNone(message.error_text)
 
-        msg_ids = [msg_id]
+        msg_ids = [message.id]
 
         res = self.resource.status(msg_ids)
         self.assertEqual(1, len(res))
         status = res[0]
-        self.assertEqual(msg_id, status['id'])
-        self.assertIsNone(status['status'])
-        self.assertIsNone(status['status_time'])
+        self.assertEqual(message.id, status.id)
+        self.assertIsNone(status.status)
+        self.assertIsNone(status.status_time)
 
         res = self.resource.delete(msg_ids)
-        self.assertEqual(msg_ids, res['deleted'])
-        self.assertTrue(res['success'])
+        self.assertEqual(msg_ids, res.deleted)
+        self.assertTrue(res.success)
