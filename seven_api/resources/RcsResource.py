a5cf88b36367e39bd5860a934b4ac2473c957670
unknown <matthiez@gmail.com>
2024-04-17 09:09:19 +0200

    add rcs dataclasses

--- seven_api/resources/RcsResource.py
+++ seven_api/resources/RcsResource.py
@@ -1,7 +1,52 @@
+from dataclasses import dataclass
+from typing import Optional, List
+
+import marshmallow_dataclass
+
 from seven_api.classes.Endpoint import Endpoint
 from seven_api.resources.Resource import Resource
 from enum import Enum
 
+
+@dataclass
+class RcsMessage:
+    channel: str
+    encoding: str
+    error: Optional[str]
+    error_text: Optional[str]
+    id: Optional[str]
+    is_binary: bool
+    label: Optional[str]
+    parts: int
+    price: float
+    recipient: str
+    sender: str
+    success: bool
+    text: str
+    udh: Optional[str]
+
+
+@dataclass
+class RcsResponse:
+    balance: float
+    debug: str
+    messages: List[RcsMessage]
+    sms_type: str
+    success: str
+    total_price: float
+
+
+dispatch_schema = marshmallow_dataclass.class_schema(RcsResponse)()
+
+@dataclass
+class RcsDeleteResponse:
+    success: bool
+delete_schema = marshmallow_dataclass.class_schema(RcsDeleteResponse)()
+
+@dataclass
+class RcsEventResponse:
+    success: bool
+event_schema = marshmallow_dataclass.class_schema(RcsEventResponse)()
 
 class RcsEvent(Enum):
     IS_TYPING = 0
@@ -14,22 +59,22 @@ class RcsTarget(str, Enum):
 
 
 class RcsResource(Resource):
-    def delete(self, msg_id: int) -> dict:
+    def delete(self, msg_id: str) -> RcsDeleteResponse:
         with self._api.client() as client:
-            return client.delete(f'{Endpoint.RCS_MESSAGES.value}/{msg_id}').json()
+            return delete_schema.loads(client.delete(f'{Endpoint.RCS_MESSAGES.value}/{msg_id}').text)
 
-    def dispatch(self, to: str, text: str, params=None) -> dict:
+    def dispatch(self, to: str, text: str, params: dict = None) -> RcsResponse:
         if params is None:
             params = {}
         params['to'] = to
         params['text'] = text
 
         with self._api.client() as client:
-            return client.post(Endpoint.RCS_MESSAGES.value, data=params).json()
+            return dispatch_schema.loads(client.post(Endpoint.RCS_MESSAGES.value, data=params).text)
 
-    def event(self, target: RcsTarget, event: RcsEvent, value, agent='') -> dict:
+    def event(self, target: RcsTarget, event: RcsEvent, value, agent='') -> RcsEventResponse:
         key = target.value
         params = {'event': event.name, key: value, 'from': agent}
 
         with self._api.client() as client:
-            return client.post(Endpoint.RCS_EVENTS.value, data=params).json()
+            return event_schema.loads(client.post(Endpoint.RCS_EVENTS.value, data=params).text)
--- tests/test_Rcs.py
+++ tests/test_Rcs.py
@@ -8,47 +8,45 @@ class TestRcs(BaseTest):
         self.resource = RcsResource(self.client)
 
     def test_delete(self) -> None:
-        message = self.resource.dispatch('+491716992343', 'Hey!', {'delay': '2050-12-31'})['messages'][0]
-        res = self.resource.delete(message['id'])
-        self.assertTrue(res['success'])
+        message = self.resource.dispatch('+491716992343', 'Hey!', {'delay': '2050-12-31'}).messages[0]
+        res = self.resource.delete(message.id)
+        self.assertTrue(res.success)
 
     def test_event__is_typing(self) -> None:
         with self.assertRaises(ValueError):
             res = self.resource.event(RcsTarget.PHONE_NUMBER, RcsEvent.IS_TYPING, "491716992343")
-            self.assertTrue(res['success'])
+            self.assertTrue(res.success)
         self.assertRaises(ValueError)
 
     def test_event__read(self) -> None:
         with self.assertRaises(ValueError):
             res = self.resource.event(RcsTarget.MESSAGE_ID, RcsEvent.READ, "123")
-            self.assertTrue(res['success'])
+            self.assertTrue(res.success)
 
     def test_text(self) -> None:
         to = '491716992343'
         text = 'Hey!'
         res = self.resource.dispatch(to, text, {'delay': '2050-12-31'})
-        self.assertEqual('100', res['success'])
-        self.assertIsInstance(res['total_price'], (float, int))
-        self.assertIsInstance(res['balance'], float)
-        self.assertIsInstance(res['debug'], str)
-        self.assertIsInstance(res['sms_type'], str)
-        self.assertIsInstance(res['messages'], list)
-        self.assertEqual(1, len(res['messages']))
-        message = res['messages'][0]
-        if 'true' == res['debug']:
-            self.assertIsNone(message['id'])
+        self.assertEqual('100', res.success)
+        self.assertGreaterEqual(res.total_price, 0.0)
+        self.assertIn(res.debug, ['true', 'false'])
+        self.assertEqual('direct', res.sms_type)
+        self.assertEqual(1, len(res.messages))
+        message = res.messages[0]
+        if 'true' == res.debug:
+            self.assertIsNone(message.id)
         else:
-            self.assertIsInstance(message['id'], str)
+            self.assertIsInstance(message.id, str)
 
-        self.assertEqual('RCS', message['channel'])
-        self.assertIsInstance(message['sender'], str)
-        self.assertEqual(to, message['recipient'])
-        self.assertEqual(text, message['text'])
-        self.assertIsInstance(message['encoding'], str)
-        self.assertIn('error', message)
-        self.assertIn('error_text', message)
-        self.assertIsInstance(message['parts'], int)
-        self.assertIsInstance(message['price'], (float, int))
-        self.assertIsInstance(message['success'], bool)
+        self.assertEqual('RCS', message.channel)
+        self.assertTrue(len(message.sender) > 0)
+        self.assertEqual(to, message.recipient)
+        self.assertEqual(text, message.text)
+        self.assertIsInstance(message.encoding, str)
+        self.assertIsNone(message.error)
+        self.assertIsNone(message.error_text)
+        self.assertEqual(1, message.parts)
+        self.assertGreaterEqual(message.price, 0.0)
+        self.assertTrue(message.success)
 
-        self.resource.delete(message['id'])
+        self.resource.delete(message.id)
