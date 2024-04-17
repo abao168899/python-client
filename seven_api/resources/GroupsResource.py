40495aee60ad4b2aa5e53ce2f97670aed5fa3f2c
unknown <matthiez@gmail.com>
2024-04-17 11:13:07 +0200

    add groups dataclasses

--- seven_api/resources/GroupsResource.py
+++ seven_api/resources/GroupsResource.py
@@ -1,11 +1,48 @@
+from dataclasses import dataclass
+from typing import List
+
+import marshmallow_dataclass
+
 from seven_api.classes.Endpoint import Endpoint
 from seven_api.classes.ToQueryString import ToQueryString
 from seven_api.resources.Resource import Resource
 
+@dataclass
+class PagingMetadata:
+    count: int
+    has_more: bool
+    limit: int
+    offset: int
+    total: int
+
+@dataclass
+class Group:
+    created: str
+    id: int
+    members_count: int
+    name: str
+group_schema = marshmallow_dataclass.class_schema(Group)()
+
+@dataclass
+class GroupsListResponse:
+    data: List[Group]
+    pagingMetadata: PagingMetadata
+
+
+list_schema = marshmallow_dataclass.class_schema(GroupsListResponse)()
+
+@dataclass
+class GroupsDeleteResponse:
+    success: bool
+
+
+delete_schema = marshmallow_dataclass.class_schema(GroupsDeleteResponse)()
+
 
 class GroupsListParams(ToQueryString):
-    limit: int = None
-    offset: int = None
+    def __init__(self, limit: int = None, offset: int = None):
+        self.limit = limit
+        self.offset = offset
 
     def __iter__(self):
         yield 'limit', self.limit
@@ -13,25 +50,29 @@ class GroupsListParams(ToQueryString):
 
 
 class GroupsResource(Resource):
-    def create(self, name: str) -> dict:
+    def create(self, name: str) -> Group:
+        payload = {'name': name}
         with self._api.client() as client:
-            return client.post(Endpoint.GROUPS.value, data={'name': name}).json()
+            return group_schema.loads(client.post(Endpoint.GROUPS.value, data=payload).text)
 
-    def delete(self, group_id: int, delete_contacts: bool = False) -> dict:
-        path = f'{Endpoint.GROUPS.value}/{group_id}'
+    def delete(self, group_id: int, delete_contacts: bool = False) -> GroupsDeleteResponse:
+        params = {}
         if delete_contacts:
-            path += f'?delete_contacts={str(delete_contacts).lower()}'
+            params.update({'delete_contacts': 'true'})
         with self._api.client() as client:
-            return client.delete(path).json()
+            return delete_schema.loads(client.delete(f'{Endpoint.GROUPS.value}/{group_id}', params=params).text)
 
-    def get(self, group_id: int) -> dict:
+    def get(self, group_id: int) -> Group:
         with self._api.client() as client:
-            return client.get(f'{Endpoint.GROUPS.value}/{group_id}').json()
+            return group_schema.loads(client.get(f'{Endpoint.GROUPS.value}/{group_id}').text)
 
-    def list(self, params: GroupsListParams) -> dict:
+    def list(self, params: GroupsListParams = None) -> GroupsListResponse:
+        if params is None:
+            params = GroupsListParams()
         with self._api.client() as client:
-            return client.get(Endpoint.GROUPS.value, params=params.as_dict()).json()
+            return list_schema.loads(client.get(Endpoint.GROUPS.value, params=params.as_dict()).text)
 
-    def update(self, group_id: int, name: str) -> dict:
+    def update(self, group_id: int, name: str) -> Group:
+        payload = {'name': name}
         with self._api.client() as client:
-            return client.patch(f'{Endpoint.GROUPS.value}/{group_id}', data={'name': name}).json()
+            return group_schema.loads(client.patch(f'{Endpoint.GROUPS.value}/{group_id}', data=payload).text)
--- tests/test_Groups.py
+++ tests/test_Groups.py
@@ -1,4 +1,4 @@
-from seven_api.resources.GroupsResource import GroupsResource, GroupsListParams
+from seven_api.resources.GroupsResource import GroupsResource, GroupsListParams, Group
 from tests.BaseTest import BaseTest
 
 
@@ -9,54 +9,50 @@ class TestGroups(BaseTest):
 
     def test_groups_delete(self) -> None:
         res_create = self.resource.create("Group Name")
-        group_id = res_create['id']
+        group_id = res_create.id
 
         try:
             self.resource.delete(group_id)
         except ValueError:
             self.fail("delete() raised ValueError unexpectedly!")
 
-    def __assert_group(self, group: dict) -> None:
-        self.assertIsNotNone(group['created'])
-        self.assertIsNotNone(group['id'])
-        self.assertIsNotNone(group['members_count'])
-        self.assertIsNotNone(group['name'])
+    def __assert_group(self, group: Group) -> None:
+        self.assertTrue(len(group.created) > 0)
+        self.assertTrue(group.id > 0)
+        self.assertTrue(group.members_count >= 0)
+        self.assertTrue(len(group.name) > 0)
 
     def test_groups_list(self) -> None:
         res_create = self.resource.create("Group Name")
 
-        params = GroupsListParams()
-        params.limit = 500
+        params = GroupsListParams(limit=500)
         res = self.resource.list(params)
-        paging_metadata = res['pagingMetadata']
-        self.assertIn('count', paging_metadata)
-        self.assertIn('has_more', paging_metadata)
-        self.assertIn('limit', paging_metadata)
-        self.assertEqual(params.limit, paging_metadata['limit'])
-        self.assertIn('offset', paging_metadata)
-        self.assertIn('total', paging_metadata)
+        self.assertTrue(res.pagingMetadata.count >= 0)
+        self.assertEqual(params.limit, res.pagingMetadata.limit)
+        self.assertTrue(res.pagingMetadata.offset >= 0)
+        self.assertTrue(res.pagingMetadata.total >= 0)
 
-        for group in res['data']:
+        for group in res.data:
             self.__assert_group(group)
 
-        self.resource.delete(res_create['id'])
+        self.resource.delete(res_create.id)
 
     def test_groups_create(self) -> None:
         name = "Group Name"
         res = self.resource.create(name)
 
-        self.assertEqual(name, res['name'])
-        self.assertEqual(0, res['members_count'])
+        self.assertEqual(name, res.name)
+        self.assertEqual(0, res.members_count)
 
-        self.resource.delete(res['id'])
+        self.resource.delete(res.id)
 
     def test_groups_get(self) -> None:
         name = "Group Name"
         create_res = self.resource.create(name)
 
-        group = self.resource.get(create_res['id'])
-        self.assertEqual(group['id'], create_res['id'])
-        self.assertEqual(name, group['name'])
+        group = self.resource.get(create_res.id)
+        self.assertEqual(group.id, create_res.id)
+        self.assertEqual(name, group.name)
         self.__assert_group(group)
 
-        self.resource.delete(create_res['id'])
+        self.resource.delete(create_res.id)
